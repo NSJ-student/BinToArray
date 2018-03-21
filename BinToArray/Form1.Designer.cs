02588d40ff59aae2ef2fab68d81e2d69f27e7a3d
NSJ-backup <ghdlghdl0558@gmail.com>
2018-03-21 23:54:13 +0900

    초기 커밋.

---
+++ BinToArray/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace BinToArray
+{
+    partial class BinToArray
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.tbLoadPath = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.tbSavePath = new System.Windows.Forms.TextBox();
+            this.rtbArrayText = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(66, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // tbLoadPath
+            // 
+            this.tbLoadPath.Location = new System.Drawing.Point(84, 14);
+            this.tbLoadPath.Name = "tbLoadPath";
+            this.tbLoadPath.ReadOnly = true;
+            this.tbLoadPath.Size = new System.Drawing.Size(236, 20);
+            this.tbLoadPath.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 41);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(66, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // tbSavePath
+            // 
+            this.tbSavePath.Location = new System.Drawing.Point(84, 43);
+            this.tbSavePath.Name = "tbSavePath";
+            this.tbSavePath.ReadOnly = true;
+            this.tbSavePath.Size = new System.Drawing.Size(236, 20);
+            this.tbSavePath.TabIndex = 3;
+            // 
+            // rtbArrayText
+            // 
+            this.rtbArrayText.Location = new System.Drawing.Point(12, 70);
+            this.rtbArrayText.Name = "rtbArrayText";
+            this.rtbArrayText.Size = new System.Drawing.Size(308, 180);
+            this.rtbArrayText.TabIndex = 4;
+            this.rtbArrayText.Text = "";
+            // 
+            // BinToArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 262);
+            this.Controls.Add(this.rtbArrayText);
+            this.Controls.Add(this.tbSavePath);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.tbLoadPath);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "BinToArray";
+            this.Text = "Bin To Array";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.TextBox tbLoadPath;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox tbSavePath;
+        private System.Windows.Forms.RichTextBox rtbArrayText;
+    }
+}
+
---
+++ BinToArray/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BinToArray
+{
+    public partial class BinToArray : Form
+    {
+        OpenFileDialog open_dialog;
+        SaveFileDialog save_dialog;
+
+        public BinToArray()
+        {
+            InitializeComponent();
+
+            open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Bin Files(*.bin)|*.bin";
+            save_dialog = new SaveFileDialog();
+            save_dialog.Filter = "Text Files(*.txt)|*.txt";
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == open_dialog.ShowDialog())
+            {
+                tbLoadPath.Text = open_dialog.FileName;
+
+                FileStream file = File.OpenRead(open_dialog.FileName);
+                BinaryReader reader = new BinaryReader(file);
+                
+                string temp = "";
+                int read = 0;
+                int pos = 0;
+
+                while(reader.BaseStream.Length - reader.BaseStream.Position >= 4)
+                {
+                    read = reader.ReadInt32();
+                    temp += "0x" + read.ToString("X") + ", ";
+                    pos++;
+                    if(pos == 4)
+                    {
+                        pos = 0;
+                        temp += "\n";
+                    }
+                }
+                rtbArrayText.Text = temp;
+
+                reader.Close();
+                file.Close();
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == save_dialog.ShowDialog())
+            {
+                tbSavePath.Text = save_dialog.FileName;
+                FileStream file = File.Create(save_dialog.FileName);
+                StreamWriter writer = new StreamWriter(file);
+
+                writer.Write(rtbArrayText.Text);
+
+                writer.Close();
+                file.Close();
+            }
+        }
+    }
+}
