44b92136054673156da602025246d32ca7e3067b
NSJ-student <ghdlghdl0558@gmail.com>
2021-04-27 14:36:56 +0900

    출력 포맷 선택 기능 추가

--- BinToArray/Form1.cs
+++ BinToArray/Form1.cs
@@ -16,6 +16,7 @@ namespace BinToArray
         OpenFileDialog open_dialog;
         SaveFileDialog save_dialog;
         BackgroundWorker LoadWorker;
+        WorkArg userArg;
 
         public BinToArray()
         {
@@ -24,6 +25,8 @@ namespace BinToArray
             progressLoad.Visible = false;
             progressLoad.Location = tbLoadPath.Location;
             progressLoad.Size = tbLoadPath.Size;
+
+            userArg = new WorkArg();
 
             open_dialog = new OpenFileDialog();
             open_dialog.Filter = "Bin Files(*.bin)|*.bin";
@@ -44,21 +47,65 @@ namespace BinToArray
 
         private void MakeComponents(object obj, DoWorkEventArgs arg)
         {
-            string FilePath = (string)arg.Argument;
+            WorkArg args = (WorkArg)arg.Argument;
+            string FilePath = (string)args.fileName;
 
-            FileStream file = File.OpenRead(open_dialog.FileName);
+            FileStream file = File.OpenRead(FilePath);
             BinaryReader reader = new BinaryReader(file);
 
             string temp = "";
-            int read = 0;
+            uint read = 0;
             int pos = 0;
+            int byteCnt = (args.showBit / 8);
 
-            while (reader.BaseStream.Length - reader.BaseStream.Position >= 4)
+            while (reader.BaseStream.Length - reader.BaseStream.Position >= byteCnt)
             {
-                read = reader.ReadInt32();
-                temp += "0x" + read.ToString("X") + ", ";
+                if(args.showBit == 8)
+                {
+                    read = reader.ReadByte();
+                }
+                else if(args.showBit == 16)
+                {
+                    read = reader.ReadUInt16();
+                }
+                else if(args.showBit == 32)
+                {
+                    read = reader.ReadUInt32();
+                }
+
+                if(args.isDec)
+                {
+                    if (args.showBit == 8)
+                    {
+                        temp += String.Format("{0,3}, ", read);
+                    }
+                    else if (args.showBit == 16)
+                    {
+                        temp += String.Format("{0,5}, ", read);
+                    }
+                    else if (args.showBit == 32)
+                    {
+                        temp += String.Format("{0,10}, ", read);
+                    }
+                }
+                else
+                {
+                    if (args.showBit == 8)
+                    {
+                        temp += "0x" + read.ToString("X2") + ", ";
+                    }
+                    else if (args.showBit == 16)
+                    {
+                        temp += "0x" + read.ToString("X4") + ", ";
+                    }
+                    else if (args.showBit == 32)
+                    {
+                        temp += "0x" + read.ToString("X8") + ", ";
+                    }
+                }
+
                 pos++;
-                if (pos == 4)
+                if (pos == args.colCount)
                 {
                     pos = 0;
                     temp += "\n";
@@ -92,7 +139,12 @@ namespace BinToArray
                 btnLoad.Enabled = false;
                 btnSave.Enabled = false;
 
-                LoadWorker.RunWorkerAsync(open_dialog.FileName);
+                if(!userArg.setProperty(open_dialog.FileName, get_isDec(), get_showBit(), (int)numColCount.Value))
+                {
+                    return;
+                }
+
+                LoadWorker.RunWorkerAsync(userArg);
             }
         }
 
@@ -110,5 +162,103 @@ namespace BinToArray
                 file.Close();
             }
         }
+
+        private bool get_isDec()
+        {
+            if(rbDec8bit.Checked || rbDec16bit.Checked || rbDec32bit.Checked)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private int get_showBit()
+        {
+            if (rbDec8bit.Checked || rbHex8bit.Checked)
+            {
+                return 8;
+            }
+            else if(rbDec16bit.Checked || rbHex16bit.Checked)
+            {
+                return 16;
+            }
+            else if(rbDec32bit.Checked || rbHex32bit.Checked)
+            {
+                return 32;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private void BtnReload_Click(object sender, EventArgs e)
+        {
+            if(tbLoadPath.Text == "")
+            {
+                return;
+            }
+
+            progressLoad.Visible = true;
+            btnLoad.Enabled = false;
+            btnSave.Enabled = false;
+
+            if (!userArg.setProperty(tbLoadPath.Text, get_isDec(), get_showBit(), (int)numColCount.Value))
+            {
+                return;
+            }
+
+            LoadWorker.RunWorkerAsync(userArg);
+        }
     }
+
+    public class WorkArg
+    {
+        string _fileName;
+        bool _isDec;
+        int _showBit;
+        int _colCount;
+
+        public WorkArg()
+        {
+            _isDec = false;
+            _showBit = 8;
+            _colCount = 8;
+        }
+        public string fileName
+        {
+            get { return _fileName; }
+        }
+        public bool isDec
+        {
+            get { return _isDec; }
+        }
+        public int showBit
+        {
+            get { return _showBit; }
+        }
+        public int colCount
+        {
+            get { return _colCount; }
+        }
+        public bool setProperty(string name, bool dec, int bit, int column)
+        {
+            _fileName = name;
+            _isDec = dec;
+            _colCount = column;
+            if ((bit == 8) || (bit == 16) || (bit == 32))
+            {
+                _showBit = bit;
+                return true;
+            }
+            else
+            {
+                _showBit = 0;
+                return false;
+            }
+        }
+    }
+
 }
