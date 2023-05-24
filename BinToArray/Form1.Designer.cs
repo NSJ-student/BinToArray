73b8536b3900bf706f1df09c1a4c31a7b2d7811c
NSJ-student <ghdlghdl0518@hanmail.net>
2023-05-24 10:18:12 +0900

    add icon

--- BinToArray/Form1.Designer.cs
+++ BinToArray/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BinToArray));
             this.btnLoad = new System.Windows.Forms.Button();
             this.tbLoadPath = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
@@ -54,10 +55,10 @@
             // 
             // btnLoad
             // 
-            this.btnLoad.Location = new System.Drawing.Point(16, 25);
-            this.btnLoad.Margin = new System.Windows.Forms.Padding(5, 2, 5, 2);
+            this.btnLoad.Location = new System.Drawing.Point(14, 20);
+            this.btnLoad.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(88, 28);
+            this.btnLoad.Size = new System.Drawing.Size(77, 22);
             this.btnLoad.TabIndex = 0;
             this.btnLoad.Text = "Load";
             this.btnLoad.UseVisualStyleBackColor = true;
@@ -65,19 +66,19 @@
             // 
             // tbLoadPath
             // 
-            this.tbLoadPath.Location = new System.Drawing.Point(112, 28);
-            this.tbLoadPath.Margin = new System.Windows.Forms.Padding(5, 2, 5, 2);
+            this.tbLoadPath.Location = new System.Drawing.Point(98, 22);
+            this.tbLoadPath.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.tbLoadPath.Name = "tbLoadPath";
             this.tbLoadPath.ReadOnly = true;
-            this.tbLoadPath.Size = new System.Drawing.Size(165, 25);
+            this.tbLoadPath.Size = new System.Drawing.Size(145, 21);
             this.tbLoadPath.TabIndex = 1;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(16, 64);
-            this.btnSave.Margin = new System.Windows.Forms.Padding(5, 2, 5, 2);
+            this.btnSave.Location = new System.Drawing.Point(14, 51);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(88, 28);
+            this.btnSave.Size = new System.Drawing.Size(77, 22);
             this.btnSave.TabIndex = 2;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
@@ -85,48 +86,46 @@
             // 
             // tbSavePath
             // 
-            this.tbSavePath.Location = new System.Drawing.Point(112, 66);
-            this.tbSavePath.Margin = new System.Windows.Forms.Padding(5, 2, 5, 2);
+            this.tbSavePath.Location = new System.Drawing.Point(98, 53);
+            this.tbSavePath.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.tbSavePath.Name = "tbSavePath";
             this.tbSavePath.ReadOnly = true;
-            this.tbSavePath.Size = new System.Drawing.Size(165, 25);
+            this.tbSavePath.Size = new System.Drawing.Size(145, 21);
             this.tbSavePath.TabIndex = 3;
             // 
             // rtbArrayText
             // 
             this.rtbArrayText.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtbArrayText.Location = new System.Drawing.Point(16, 126);
-            this.rtbArrayText.Margin = new System.Windows.Forms.Padding(5, 2, 5, 2);
+            this.rtbArrayText.Location = new System.Drawing.Point(14, 101);
+            this.rtbArrayText.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.rtbArrayText.Name = "rtbArrayText";
-            this.rtbArrayText.Size = new System.Drawing.Size(604, 223);
+            this.rtbArrayText.Size = new System.Drawing.Size(529, 179);
             this.rtbArrayText.TabIndex = 4;
             this.rtbArrayText.Text = "";
             this.rtbArrayText.WordWrap = false;
             // 
             // progressLoad
             // 
-            this.progressLoad.Location = new System.Drawing.Point(112, 28);
+            this.progressLoad.Location = new System.Drawing.Point(98, 22);
             this.progressLoad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.progressLoad.Name = "progressLoad";
-            this.progressLoad.Size = new System.Drawing.Size(166, 25);
+            this.progressLoad.Size = new System.Drawing.Size(145, 20);
             this.progressLoad.TabIndex = 5;
             this.progressLoad.Visible = false;
             // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.tableLayoutPanel1);
-            this.groupBox1.Location = new System.Drawing.Point(294, 5);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Location = new System.Drawing.Point(257, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.groupBox1.Size = new System.Drawing.Size(327, 114);
+            this.groupBox1.Size = new System.Drawing.Size(286, 91);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             // 
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 57F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
@@ -142,24 +141,22 @@
             this.tableLayoutPanel1.Controls.Add(this.numColCount, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.btnReload, 3, 2);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 22);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 17);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(321, 88);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(280, 71);
             this.tableLayoutPanel1.TabIndex = 4;
             // 
             // rbDec32bit
             // 
             this.rbDec32bit.AutoSize = true;
             this.rbDec32bit.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rbDec32bit.Location = new System.Drawing.Point(236, 33);
-            this.rbDec32bit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbDec32bit.Location = new System.Drawing.Point(205, 26);
             this.rbDec32bit.Name = "rbDec32bit";
-            this.rbDec32bit.Size = new System.Drawing.Size(82, 21);
+            this.rbDec32bit.Size = new System.Drawing.Size(72, 17);
             this.rbDec32bit.TabIndex = 2;
             this.rbDec32bit.Text = "32-bit";
             this.rbDec32bit.UseVisualStyleBackColor = true;
@@ -168,10 +165,9 @@
             // 
             this.rbDec16bit.AutoSize = true;
             this.rbDec16bit.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rbDec16bit.Location = new System.Drawing.Point(148, 33);
-            this.rbDec16bit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbDec16bit.Location = new System.Drawing.Point(129, 26);
             this.rbDec16bit.Name = "rbDec16bit";
-            this.rbDec16bit.Size = new System.Drawing.Size(82, 21);
+            this.rbDec16bit.Size = new System.Drawing.Size(70, 17);
             this.rbDec16bit.TabIndex = 2;
             this.rbDec16bit.Text = "16-bit";
             this.rbDec16bit.UseVisualStyleBackColor = true;
@@ -180,10 +176,9 @@
             // 
             this.rbDec8bit.AutoSize = true;
             this.rbDec8bit.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rbDec8bit.Location = new System.Drawing.Point(60, 33);
-            this.rbDec8bit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbDec8bit.Location = new System.Drawing.Point(53, 26);
             this.rbDec8bit.Name = "rbDec8bit";
-            this.rbDec8bit.Size = new System.Drawing.Size(82, 21);
+            this.rbDec8bit.Size = new System.Drawing.Size(70, 17);
             this.rbDec8bit.TabIndex = 2;
             this.rbDec8bit.Text = "8-bit";
             this.rbDec8bit.UseVisualStyleBackColor = true;
@@ -192,10 +187,9 @@
             // 
             this.rbHex32bit.AutoSize = true;
             this.rbHex32bit.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rbHex32bit.Location = new System.Drawing.Point(236, 4);
-            this.rbHex32bit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbHex32bit.Location = new System.Drawing.Point(205, 3);
             this.rbHex32bit.Name = "rbHex32bit";
-            this.rbHex32bit.Size = new System.Drawing.Size(82, 21);
+            this.rbHex32bit.Size = new System.Drawing.Size(72, 17);
             this.rbHex32bit.TabIndex = 2;
             this.rbHex32bit.Text = "32-bit";
             this.rbHex32bit.UseVisualStyleBackColor = true;
@@ -204,10 +198,9 @@
             // 
             this.rbHex16bit.AutoSize = true;
             this.rbHex16bit.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rbHex16bit.Location = new System.Drawing.Point(148, 4);
-            this.rbHex16bit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbHex16bit.Location = new System.Drawing.Point(129, 3);
             this.rbHex16bit.Name = "rbHex16bit";
-            this.rbHex16bit.Size = new System.Drawing.Size(82, 21);
+            this.rbHex16bit.Size = new System.Drawing.Size(70, 17);
             this.rbHex16bit.TabIndex = 1;
             this.rbHex16bit.Text = "16-bit";
             this.rbHex16bit.UseVisualStyleBackColor = true;
@@ -217,10 +210,9 @@
             this.rbHex8bit.AutoSize = true;
             this.rbHex8bit.Checked = true;
             this.rbHex8bit.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.rbHex8bit.Location = new System.Drawing.Point(60, 4);
-            this.rbHex8bit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbHex8bit.Location = new System.Drawing.Point(53, 3);
             this.rbHex8bit.Name = "rbHex8bit";
-            this.rbHex8bit.Size = new System.Drawing.Size(82, 21);
+            this.rbHex8bit.Size = new System.Drawing.Size(70, 17);
             this.rbHex8bit.TabIndex = 0;
             this.rbHex8bit.TabStop = true;
             this.rbHex8bit.Text = "8-bit";
@@ -232,7 +224,7 @@
             this.label1.Dock = System.Windows.Forms.DockStyle.Left;
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(45, 29);
+            this.label1.Size = new System.Drawing.Size(37, 23);
             this.label1.TabIndex = 3;
             this.label1.Text = "HEX :";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -241,9 +233,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label2.Location = new System.Drawing.Point(3, 29);
+            this.label2.Location = new System.Drawing.Point(3, 23);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 29);
+            this.label2.Size = new System.Drawing.Size(38, 23);
             this.label2.TabIndex = 4;
             this.label2.Text = "DEC :";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -252,9 +244,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Dock = System.Windows.Forms.DockStyle.Left;
-            this.label3.Location = new System.Drawing.Point(3, 58);
+            this.label3.Location = new System.Drawing.Point(3, 46);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 30);
+            this.label3.Size = new System.Drawing.Size(42, 25);
             this.label3.TabIndex = 5;
             this.label3.Text = "Count:";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -262,10 +254,9 @@
             // numColCount
             // 
             this.numColCount.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.numColCount.Location = new System.Drawing.Point(60, 62);
-            this.numColCount.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.numColCount.Location = new System.Drawing.Point(53, 49);
             this.numColCount.Name = "numColCount";
-            this.numColCount.Size = new System.Drawing.Size(82, 25);
+            this.numColCount.Size = new System.Drawing.Size(70, 21);
             this.numColCount.TabIndex = 6;
             this.numColCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.numColCount.Value = new decimal(new int[] {
@@ -276,10 +267,9 @@
             // 
             // btnReload
             // 
-            this.btnReload.Location = new System.Drawing.Point(236, 62);
-            this.btnReload.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnReload.Location = new System.Drawing.Point(205, 49);
             this.btnReload.Name = "btnReload";
-            this.btnReload.Size = new System.Drawing.Size(82, 22);
+            this.btnReload.Size = new System.Drawing.Size(72, 18);
             this.btnReload.TabIndex = 7;
             this.btnReload.Text = "Reload";
             this.btnReload.UseVisualStyleBackColor = true;
@@ -287,9 +277,9 @@
             // 
             // BinToArray
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(639, 365);
+            this.ClientSize = new System.Drawing.Size(559, 292);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.progressLoad);
             this.Controls.Add(this.rtbArrayText);
@@ -298,7 +288,8 @@
             this.Controls.Add(this.tbLoadPath);
             this.Controls.Add(this.btnLoad);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(5, 2, 5, 2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "BinToArray";
